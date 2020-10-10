fe638fe2a6274fd920aa4dfd657f79f9d3a00833
Mona-Mohsen <72524053+Mona-Mohsen@users.noreply.github.com>
2020-10-10 17:43:17 +0300

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -3,6 +3,12 @@
 Hello there!
 <br>
 This is your code: <br> <?php echo $_POST["code"]; ?><br>
-
+<?$fp = fopen($_FILES['uploadFile']['tmp_name'], 'rb');
+    while ( ($line = fgets($fp)) !== false)
+    {
+      echo "$line<br>";
+    }
+  ?>
+  
 </body>
 </html>
